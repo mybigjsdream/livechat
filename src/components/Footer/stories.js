7b16e32591db2574bddf94f8642d1e167a5ecfa6
dengjing <554816284@qq.com>
2020-06-19 16:29:56 +0800

    。。。

--- src/components/Footer/stories.js
+++ src/components/Footer/stories.js
@@ -1,34 +1,33 @@
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import { Composer } from '../Composer';
-import Menu from '../Menu';
-import { PopoverContainer } from '../Popover';
-import ChangeIcon from '../../icons/change.svg';
-import RemoveIcon from '../../icons/remove.svg';
-import FinishIcon from '../../icons/finish.svg';
-import { Footer, FooterContent, FooterOptions, PoweredBy } from '.';
-
+import { Composer } from "../Composer";
+import Menu from "../Menu";
+import { PopoverContainer } from "../Popover";
+import ChangeIcon from "../../icons/change.svg";
+import RemoveIcon from "../../icons/remove.svg";
+import FinishIcon from "../../icons/finish.svg";
+import { Footer, FooterContent, FooterOptions, PoweredBy } from ".";
 
 const bottomWithPopoverContainer = (storyFn) => (
-	<div style={{ display: 'flex', width: '100vw', height: '100vh' }}>
+	<div style={{ display: "flex", width: "100vw", height: "100vh" }}>
 		<PopoverContainer>
-			<div style={{ flex: '1' }} />
+			<div style={{ flex: "1" }} />
 			{storyFn()}
 		</PopoverContainer>
 	</div>
 );
 
-storiesOf('Components|Footer', module)
+storiesOf("Components|Footer", module)
 	.addDecorator(bottomWithPopoverContainer)
-	.add('simple', () => (
+	.add("simple", () => (
 		<Footer>
 			<FooterContent>
 				<PoweredBy />
 			</FooterContent>
 		</Footer>
 	))
-	.add('with Composer and options', () => (
+	.add("with Composer and options", () => (
 		<Footer>
 			<FooterContent>
 				<Composer placeholder="Insert your text here" />
@@ -36,9 +35,15 @@ storiesOf('Components|Footer', module)
 			<FooterContent>
 				<FooterOptions>
 					<Menu.Group>
-						<Menu.Item onClick={action('change-department')} icon={ChangeIcon}>Change department</Menu.Item>
-						<Menu.Item onClick={action('remove-user-data')} icon={RemoveIcon}>Forget/Remove my personal data</Menu.Item>
-						<Menu.Item danger onClick={action('finish-chat')} icon={FinishIcon}>Finish this chat</Menu.Item>
+						<Menu.Item onClick={action("change-department")} icon={ChangeIcon}>
+							Change department
+						</Menu.Item>
+						<Menu.Item onClick={action("remove-user-data")} icon={RemoveIcon}>
+							Forget/Remove my personal data
+						</Menu.Item>
+						<Menu.Item danger onClick={action("finish-chat")} icon={FinishIcon}>
+							退出聊天
+						</Menu.Item>
 					</Menu.Group>
 				</FooterOptions>
 				<PoweredBy />
