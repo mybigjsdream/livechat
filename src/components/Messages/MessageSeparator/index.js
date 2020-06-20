0513c4d5a1398dcd43452792d82331bcc2d08d7a
dengjing <554816284@qq.com>
2020-06-20 09:58:27 +0800

    汉化下

--- src/components/Messages/MessageSeparator/index.js
+++ src/components/Messages/MessageSeparator/index.js
@@ -1,33 +1,31 @@
-import format from 'date-fns/format';
-import { parseISO } from 'date-fns/fp';
+import format from "date-fns/format";
+import { parseISO } from "date-fns/fp";
 
-import { createClassName, memo } from '../../helpers';
-import styles from './styles.scss';
+import { createClassName, memo } from "../../helpers";
+import styles from "./styles.scss";
 
-
-export const MessageSeparator = memo(({
-	date,
-	unread,
-	use: Element = 'div',
-	className,
-	style = {},
-}) => (
-	<Element
-		className={createClassName(styles, 'separator', {
-			date: !!date && !unread,
-			unread: !date && !!unread,
-		}, [className])}
-		style={style}
-	>
-		<hr className={createClassName(styles, 'separator__line')} />
-		{(date || unread) && (
-			<span className={createClassName(styles, 'separator__text')}>
+export const MessageSeparator = memo(
+	({ date, unread, use: Element = "div", className, style = {} }) => (
+		<Element
+			className={createClassName(
+				styles,
+				"separator",
 				{
-					(!!date && format(parseISO(date), 'MMM dd, yyyy').toUpperCase())
-					|| (unread && I18n.t('unread messages'))
-				}
-			</span>
-		)}
-		<hr className={createClassName(styles, 'separator__line')} />
-	</Element>
-));
+					date: !!date && !unread,
+					unread: !date && !!unread,
+				},
+				[className]
+			)}
+			style={style}
+		>
+			<hr className={createClassName(styles, "separator__line")} />
+			{(date || unread) && (
+				<span className={createClassName(styles, "separator__text")}>
+					{(!!date && format(parseISO(date), "yyyy/MM/dd").toUpperCase()) ||
+						(unread && I18n.t("unread messages"))}
+				</span>
+			)}
+			<hr className={createClassName(styles, "separator__line")} />
+		</Element>
+	)
+);
